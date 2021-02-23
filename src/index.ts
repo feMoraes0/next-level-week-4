ebbe5cb877c5f9c30d955df5172efe5e8d8a821c
feMoraes0 <fernandomoraes.lopes@gmail.com>
2021-02-22 21:53:59 -0300

    Creates the first route

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,10 @@ import express from 'express';
 const app = express();
 const port = 3333;
 
+app.get("/basic", (request, response) => {
+  return response.json({message: "Basic route created!"});
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}!`);
 });
